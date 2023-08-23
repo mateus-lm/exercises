17150319ec78bc218a878153a9696cf0744a7c60
Mateus Moreira <mateuuusss@gmail.com>
2023-08-23 15:48:53 -0300

    Simples teste de loops em js

---
+++ js/forAndLoops.js
@@ -0,0 +1,26 @@
+// const passengers = {
+//     name: ['Will Alexander',
+//         'Sarah Kate',
+//         'Audrey Simon',
+//         'Tau Perkington',],
+//     seatbelt: [12, 13, 15]
+// }
+// for (let i in passengers) {
+//     console.log('Boarding passenger ' + passengers[i] + " @ " + i);
+// }
+
+// const passengers = [
+//     'Will Alexander',
+//     'Sarah Kate',
+//     'Audrey Simon',
+//     'Tau Perkington'
+// ]
+// for (let passenger of passengers) {
+//     console.log('Boarding passenger ' + passenger);
+// }
+
+let i = 0
+while (i<10){
+i++;
+console.log(i);
+}
